dc33f2c07c1b02379cfcb4d0d75ef17ee7fe9002
JoseB91 <jabro.pipoec@gmail.com>
2026-01-18 12:31:21 -0500

    Initial Commit

---
+++ LiquidGlass/LiquidGlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassApp.swift
+//  LiquidGlass
+//
+//  Created by José Briones on 18/1/26.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
